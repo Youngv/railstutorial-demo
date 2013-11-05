31974eac89b6187f16a80ed41c28db8d6adfeb93
Youngv <ootyoungtoo@gmail.com>
2013-11-05 17:03:46 +0800

    test the full_title helper method

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "full title" do
+  it "should include the page title" do
+    expect(full_title('Home')).to match(/Home/)  
+    expect(full_title('')).to match(/Ruby on Rails Tutorial Demo/)  
+  end
+
+  it "should include the base title" do
+    expect(full_title('Contact')).to match(/^Ruby on Rails Tutorial Demo/)
+  end
+
+  it "should only include the base title" do
+    expect(full_title('')).to match(/^Ruby on Rails Tutorial Demo/)
+  end
+end
